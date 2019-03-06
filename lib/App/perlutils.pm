8f22bcedf2671fe2a56107a1ce2f0f58ca3b5119
Magnus Woldrich <m@japh.se>
2019-03-06 08:01:09 +0100

    add package App::perlutils

---
+++ lib/App/perlutils.pm
@@ -0,0 +1,71 @@
+package App::perlutils; 
+use strict;
+
+our $VERSION = '0.020';
+
+
+1;
+
+
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 EXPORTS
+
+None by default.
+
+=head1 FUNCTIONS
+
+=head2 foo()
+
+=over 4
+
+=item    Arguments: $string
+
+=item Return value: \%hash
+
+=back
+
+Description.
+
+=head1 REPORTING BUGS
+
+Report bugs and/or feature requests on rt.cpan.org, the repository issue tracker
+or directly to L<m@japh.se>
+
+=head1 AUTHOR
+
+  Magnus Woldrich
+  CPAN ID: WOLDRICH
+  m@japh.se
+  L<~/|http://japh.se>
+  L<git|http://github.com/trapd00r>
+
+=head1 CONTRIBUTORS
+
+None required yet.
+
+=head1 COPYRIGHT
+
+Copyright 2019- B<THIS MODULE>s L</AUTHOR> and L</CONTRIBUTORS> as listed above.
+
+=head1 LICENSE
+
+This library is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<~/|http://japh.se>
+
+=cut
+
